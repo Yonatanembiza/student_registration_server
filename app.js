311673bc327555d15a0a462e6e3af9040dd20f50
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2023-12-17 17:08:53 -0600

    Errors corrected

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// let express = require("express")
-// let studentRouter = require("./router/studentRouter");
-// let app = express();
-
-// app.use(express.json())
-
-// app.use("/students", studentRouter)
-
-// app.listen(3000, () => {
-//     console.log('Server is started on 3000...');
-// })
-
 const express = require("express");
 const bodyParser = require('body-parser');
 const studentRouter = require("./router/studentRouter");
